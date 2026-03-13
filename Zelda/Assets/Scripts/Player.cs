2ffc49b77213cacfe5e2a731441be039ab2ae539
Professor Aécio <professoraecio@gmail.com>
2026-03-12 21:47:23 -0300

    Finalizar config movimento player

--- Zelda/Assets/Scripts/Player.cs
+++ Zelda/Assets/Scripts/Player.cs
@@ -1,16 +1,65 @@
+using System;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [Header("Movimento")]
+    public float velocidade = 6f;
+    [Header("Joystick")]
+    [Range(0f,0.5f)] public float deadzone = 0.15f;
+    public bool usarDpadComoDigital = true;
+    private Vector3 direcao;
+
+    private CharacterController controller;
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float dirX = 0f;
+        float dirY = 0f;
+
+        // Teste input teclado
+        if(Keyboard.current != null)
+        {
+            if(Keyboard.current.leftArrowKey.isPressed)
+                dirX -= 1f;
+            if(Keyboard.current.rightArrowKey.isPressed)
+                dirX += 1f;
+            if(Keyboard.current.downArrowKey.isPressed)
+                dirY -= 1f;
+            if(Keyboard.current.upArrowKey.isPressed)
+                dirY += 1f;
+        }
+
+        // Teste input joystick
+        var gp = Gamepad.current;
+        if(gp != null)
+        {
+            Vector2 stick = gp.leftStick.ReadValue();
+            if(Math.Abs(stick.x) > deadzone) dirX += stick.x;
+            if(Math.Abs(stick.y) > deadzone) dirY += stick.y;
+
+            if(usarDpadComoDigital)
+            {
+                if(gp.dpad.left.isPressed) dirX -= 1f;
+                if(gp.dpad.right.isPressed) dirX += 1f;
+                if(gp.dpad.down.isPressed) dirY -= 1f;
+                if(gp.dpad.up.isPressed) dirY += 1f;
+            }
+        }
+
+        dirX = Mathf.Clamp(dirX,-1f,1f);
+        dirY = Mathf.Clamp(dirY,-1f,1f);
+
+        // Aplicar movimento independente
+        // se veio do joystick ou teclado
+        direcao = new Vector3(dirX,0,dirY).normalized;
+        controller.Move(direcao * velocidade * Time.deltaTime);
+
     }
 }
